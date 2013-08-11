1dd75adbf8b56d1ef95eedbab8814c5783d1167a
Marcin Kostrzewa <marcinkostrzewa@yahoo.com>
2013-08-11 23:18:53 +0200

    primitive commands dispatcher

---
+++ game/game_test.go
@@ -0,0 +1,18 @@
+package main
+
+import (
+  "testing"
+  "github.com/stretchr/testify/assert"
+  "fmt"
+)
+
+func TestCommandDispatching (t *testing.T) {
+  g := BuildGame()
+  assert.Equal(t, g.runCommand("UNKNOWN_COMMAND 0 1 2"), "ERROR 1 INVALID COMMAND")
+  g.registerCommand("UNKNOWN_COMMAND", 3, func (a []string) string {
+    return fmt.Sprintf("Function called with %s %s and %s", a[0], a[1], a[2])
+  })
+  assert.Equal(t, g.runCommand("UNKNOWN_COMMAND 0 1 2"), "Function called with 0 1 and 2")
+  assert.Equal(t, g.runCommand("UNKNOWN_COMMAND 1 2"), "ERROR 2 INVALID NUMBER OF ARGUMENTS")
+  assert.Equal(t, g.runCommand("UNKNOWN_COMMAND"), "ERROR 2 INVALID NUMBER OF ARGUMENTS")
+}
